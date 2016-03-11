a6e53e02a61fb2515b2c2cd0e33ae270f8bdd95e
Derek Tremblay <derektremblay666@gmail.com>
2016-03-11 07:40:29 -0500

    ByteProvider class

---
+++ WPFHexaEditor.Control/Core/ByteProvider.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFHexaEditor.Control.Core
+{
+    public class ByteProvider
+    {
+        //TODO : MAke class and implementing in hexaeditor
+
+        //position
+        //filename
+        //filestream
+        //openfile
+        //closefile
+        //byteaction list
+        //addbyte
+        //deletebyte
+        //lenght
+        //getbyte
+        //...
+           
+    }
+}

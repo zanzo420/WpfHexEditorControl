9055f07c53551253d87d4fc534631c5c096da0ca
Derek Tremblay <khenshin@msn.com>
2017-03-09 22:53:54 -0500

    Starting implementation of TBLEditor

--- WPFHexaEditor.Control/TBLEntryControl.xaml.cs
+++ WPFHexaEditor.Control/TBLEntryControl.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WPFHexaEditor.Core.TBL;
 
 namespace WPFHexaEditor.Control
 {
@@ -19,9 +20,49 @@ namespace WPFHexaEditor.Control
     /// </summary>
     internal partial class TBLEntryControl : UserControl
     {
+        private DTEType _dteType;
+
         public TBLEntryControl()
         {
             InitializeComponent();
+        }
+
+        private DTEType Type
+        {
+            get
+            {
+                return this._dteType;
+            }
+            set
+            {
+                //switch (value)
+                //{
+                //    case DTEType.DualTitleEncoding:
+                //        picType.Image = ListImageTBL.Images[0];
+                //        this._dteType = DTEType.DualTitleEncoding;
+                //        break;
+                //    case DTEType.ASCII:
+                //        picType.Image = ListImageTBL.Images[2];
+                //        this._dteType = DTEType.ASCII;
+                //        break;
+                //    case DTEType.MultipleTitleEncoding:
+                //        picType.Image = ListImageTBL.Images[1];
+                //        this._dteType = DTEType.MultipleTitleEncoding;
+                //        break;
+                //    case DTEType.EndBlock:
+                //        picType.Image = ListImageTBL.Images[4];
+                //        this._dteType = DTEType.EndBlock;
+                //        break;
+                //    case DTEType.EndLine:
+                //        picType.Image = ListImageTBL.Images[4];
+                //        this._dteType = DTEType.EndLine;
+                //        break;
+                //    case DTEType.Japonais:
+                //        picType.Image = ListImageTBL.Images[3];
+                //        this._dteType = DTEType.Japonais;
+                //        break;
+                //}
+            }
         }
     }
 }

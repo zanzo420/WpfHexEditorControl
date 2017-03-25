e3a28e05f2b02c066e858bec3f34064c1f8b9aa2
Derek Tremblay <khenshin@msn.com>
2017-03-24 22:39:13 -0400

    Starting work on TBL file editor.

--- WPFHexaEditor.Control/TBLEditor.xaml.cs
+++ WPFHexaEditor.Control/TBLEditor.xaml.cs
@@ -3,7 +3,10 @@
 // Author : Derek Tremblay (derektremblay666@gmail.com)
 //////////////////////////////////////////////
 
+using System;
+using System.Windows;
 using System.Windows.Controls;
+using WPFHexaEditor.Core.CharacterTable;
 
 namespace WPFHexaEditor.Control
 {
@@ -16,5 +19,34 @@ namespace WPFHexaEditor.Control
         {
             InitializeComponent();
         }
+
+        #region Properties
+        public TBLStream TBL
+        {
+            get { return (TBLStream)GetValue(TBLProperty); }
+            set { SetValue(TBLProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for TBL.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TBLProperty =
+            DependencyProperty.Register("TBL", typeof(TBLStream), typeof(TBLEditor),
+                new FrameworkPropertyMetadata(null, new PropertyChangedCallback(TBL_Changed)));
+
+        private static void TBL_Changed(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            
+        }
+
+        #endregion Properties
+
+        #region methods
+        public void Load()
+        {
+            if(TBL != null)
+            {
+                //Load section and content
+            }
+        }
+        #endregion methods
     }
 }

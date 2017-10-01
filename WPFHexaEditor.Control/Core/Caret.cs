33eb43f18608a5f22c379319776bfb3b9f99a804
Derek Tremblay <khenshin@msn.com>
2017-10-01 14:46:15 -0400

    Added a caret to the control.

---
+++ WPFHexaEditor.Control/Core/Caret.cs
@@ -0,0 +1,81 @@
+﻿//////////////////////////////////////////////
+// Fork : Derek Tremblay (derektremblay666@gmail.com) 
+// Reference : https://www.codeproject.com/Tips/431000/Caret-for-WPF-User-Controls
+// Reference license : The Code Project Open License (CPOL) 1.02
+//////////////////////////////////////////////
+
+using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Threading;
+
+namespace WpfHexaEditor.Core
+{
+    public class Caret : FrameworkElement
+    {
+        private Timer timer;
+        private Point _location;
+        private int _blinkPeriod = 500;
+        private readonly Pen _pen = new Pen(Brushes.Black, 1);
+
+ 
+        public Caret()
+        {
+            _pen.Freeze();
+            CaretHeight = 18;
+            Visible = true;
+            timer = new Timer(BlinkCaret, null, 0, _blinkPeriod);
+        }
+
+        protected override void OnRender(DrawingContext dc)
+        {
+            if (Visible)
+                dc.DrawLine(_pen, _location, new Point(Left, _location.Y + CaretHeight));
+        }
+
+        public static readonly DependencyProperty VisibleProperty =
+            DependencyProperty.Register("Visible", typeof(bool),
+                typeof(Caret), new FrameworkPropertyMetadata(false, FrameworkPropertyMetadataOptions.AffectsRender));
+        
+        public bool Visible
+        {
+            get => (bool)GetValue(VisibleProperty);
+            set => SetValue(VisibleProperty, value);
+        }
+
+        private void BlinkCaret(Object state) => Dispatcher.Invoke(delegate { Visible = !Visible; });
+
+        private double CaretHeight { get; }
+
+        public double Left
+        {
+            get => _location.X;
+            set
+            {
+                if (_location.X == value) return;
+
+                _location.X = Math.Floor(value) + .5; //to avoid WPF antialiasing
+                if (Visible)
+                {
+                    Visible = false;
+                }
+            }
+        }
+
+        public double Top
+        {
+            get => _location.Y;
+            set
+            {
+                if (_location.Y != value)
+                {
+                    _location.Y = Math.Floor(value) + .5; //to avoid WPF antialiasing
+                    if (Visible)
+                    {
+                        Visible = false;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ WPFHexaEditor.Control/Core/MethodExtention/StringExtension.cs
@@ -0,0 +1,59 @@
+﻿//////////////////////////////////////////////
+// MIT License  - 2016-2017
+// Author : Derek Tremblay (derektremblay666@gmail.com)
+//////////////////////////////////////////////
+
+
+using System;
+using System.Text.RegularExpressions;
+
+namespace WPFHexaEditor.Core.MethodExtention
+{
+    public static class StringExtension
+    {
+        /// <summary>
+        /// Indique si l'adresse email est valide
+        /// </summary>        
+        public static bool IsValidEmailAddress(this string s)
+        {
+            Regex regex = new Regex(@"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$");
+            return regex.IsMatch(s);
+        }
+
+        /// <summary>
+        /// Retourne le nombre de mot contenue dans la string
+        /// </summary>        
+        public static int WordCount(this String str)
+        {
+            return str.Split(new char[] { ' ', '.', '?' }, StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+
+        /// <summary>
+        /// Retourne True si le chaine est numerique
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static bool IsNumeric(this string s)
+        {
+            long test = 0;
+
+            try
+            {
+                test = Convert.ToInt64(s);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Retourne seulement un nombre sous forme de string contenue la string
+        /// </summary>        
+        public static string GetNumberInString(this string s)
+        {
+                return Regex.Match(s, @"\d+").Value;
+        }
+    }
+}

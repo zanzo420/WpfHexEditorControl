fb2e62dcb15efb52479dc57afa73d686cb8d9f6e
Jake Franks <1250244560@qq.com>
2017-11-29 13:39:05 +0800

    1.Shell.xaml.cs added. 2.prism:ViewModelLocator.AutoWireViewModel="True" Implemented.

---
+++ WpfHexEditor.Sample.MVVM/Views/Shell.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfHexEditor.Sample.MVVM.ViewModels;
+
+namespace WpfHexEditor.Sample.MVVM.Views {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class Shell : Window {
+        public Shell() {
+            InitializeComponent();
+            this.DataContext = new ShellViewModel();
+        }
+
+        //public void AddInputBinding(InputBinding ib) => this.InputBindings.Add(ib);
+    }
+}

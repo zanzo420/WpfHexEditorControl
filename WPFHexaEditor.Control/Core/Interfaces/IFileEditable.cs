bbdc8bfb59ac4c928775331a10e7e0de42dc8d9a
Jake Franks <1250244560@qq.com>
2017-12-03 15:10:02 +0800

    1.IFileEditable added.

---
+++ WPFHexaEditor.Control/Core/Interfaces/IFileEditable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfHexaEditor.Core.Interfaces {
+    public interface IFileEditable {
+        string FileName { get; set; }
+        void CloseProvider();
+        void SubmitChanges(string newfilename, bool overwrite = false);
+        void SubmitChanges();
+
+        
+    }
+}

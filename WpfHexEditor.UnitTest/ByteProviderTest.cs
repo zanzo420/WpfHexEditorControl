fcd604fbd940128eaff18117f5a40852ba530ab2
Derek Tremblay <khenshin@msn.com>
2018-01-04 19:23:16 -0500

    Added a UnitTest project

---
+++ WpfHexEditor.UnitTest/ByteProviderTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WpfHexaEditor.Core.Bytes;
+
+namespace HexEditUnitTest
+{
+    [TestClass]
+    public class ByteProviderTest
+    {
+        [TestMethod]
+        public void GetByteCountTest()
+        {
+            var bp = new ByteProvider(@"C:\Test\TestFile.smc");
+
+            var lenght = bp.Length;
+            var byteCount = bp.GetByteCount().Sum();
+
+            Assert.AreEqual(lenght, byteCount, "The number of byte need to be equal!");
+        }
+        
+
+    }
+}

1ffe5c84f49137d0b5ba5183d9351318e03c0968
Jake Franks <1250244560@qq.com>
2018-02-06 09:48:47 +0800

    IDataLayer,GenricStaticInstance,DrawedHexEditor.xaml.cs commit.

---
+++ WPFHexaEditor.Control/Core/GenericStaticInstance.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfHexaEditor.Core {
+    public abstract class GenericStaticInstance<T> where T:class,new() {
+        private static T _staticInstance;
+        public static T StaticInstance => _staticInstance ?? (_staticInstance = new T());
+    }
+}
